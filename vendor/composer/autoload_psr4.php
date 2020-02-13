c4f682073ec51eed5f24a44df94bfc7307ad4ea0
Douglas Oliveira R <douglasor@gmail.com>
2020-02-13 19:18:38 -0300

    Seeds e Faker

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -16,4 +16,5 @@ return array(
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Phinx\\' => array($vendorDir . '/robmorgan/phinx/src/Phinx'),
     'Financas\\' => array($baseDir . '/src'),
+    'Faker\\' => array($vendorDir . '/fzaninotto/faker/src/Faker'),
 );
